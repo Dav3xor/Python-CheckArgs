3c84509865cf6ab054c5e6d94b1c0f89e4d1215b
David Case <dave@bigbooty.(none)>
2010-04-23 22:10:15 -0700

    added extended check args decorator

--- checkargs.py
+++ checkargs.py
@@ -5,9 +5,7 @@ class BadArgument(Exception):
    return repr(self.parameter)
 
 
-
-
-class checkargs(object):
+class check_args(object):
   def __init__(self, *args):
     self.types = args
 
@@ -19,12 +17,42 @@ class checkargs(object):
       function(*args)
     return check_arguments
 
-@checkargs(str,int,float)
+
+class check_args_ex(object):
+  def __init__(self, *args):
+    self.types = args
+
+  def docondition(self, arg, condition):
+    if type(condition) == xrange and (type(arg) != int or arg not in condition):
+      raise BadArgument(str(arg) + " is not in " + str(condition)) 
+    
+    elif type(condition) in (list,tuple) and arg not in condition:
+      raise BadArgument(str(arg) + " is not in " + str(condition)) 
+
+    elif type(condition) == type and type(arg) != condition:
+      raise BadArgument(str(arg) + " is not of type " + str(condition)) 
+      
+
+  def __call__(self, function):
+    def check_arguments(*args):
+      for arg, curtype in zip(args,self.types):
+        if type(curtype) in (tuple,list):
+          for condition in curtype:
+            self.docondition(arg,condition)
+        elif type(arg) != curtype:
+          raise BadArgument(str(arg) + " is not of type " + str(curtype)) 
+      function(*args)
+    return check_arguments
+
+
+
+@check_args(str,int,float)
+@check_args_ex(str, (int,[1,2,3,4,5],xrange(3,8)), float)
 def func(name,numstuff,percent):
   print "%s %d %f" % (name,numstuff,percent)
 
 
-func("hello", 1.2, 1.5)
+func("hello", 2, 1.5)
 
 
 
