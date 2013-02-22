4f620407875c2d7f790dbd1ac3e340f512d8bdb1
David Case <Dav3xor@gmail.com>
2013-02-22 09:42:59 -0800

    added better conditional and type checking

--- checkargs.py
+++ checkargs.py
@@ -1,8 +1,11 @@
+import types
+from pprint import pprint
+
 class BadArgument(Exception):
- def __init__(self, value):
-   self.parameter = value
- def __str__(self):
-   return repr(self.parameter)
+  def __init__(self, value):
+    self.parameter = value
+  def __str__(self):
+    return repr(self.parameter)
 
 
 class check_args(object):
@@ -28,48 +31,105 @@ class check_args(object):
       function(*args)
     return check_arguments
 
+# need a function for raising exceptions inside of
+# list comprehensions, etc.
 def bail(error):
   raise BadArgument(error)
 
 class check_args_ex(object):
   """
-  Extended version of check_args, it allows you to test against multiple
-  conditions.  Handier, but takes more time.
+  Extended version of check_args, you can allow multiple types
+  for each argument (both int and float for instance), and
+  also specify conditions for each argument.
 
-  >>> @check_args_ex(str,(int,xrange(10)))
-  ... def dostuff2(name, age):
-  ...     print "name = %s age = %d" % (name,age)
- 
-  >>> dostuff2("Dave",5)
-  name = Dave age = 5
+  conditional checking can check to see if an integer is
+  within an xrange, or in a list/tuple/dictionary.
+
+  WARNING:  If you want to pass in a single list or tuple,
+  it must be enclosed in another list or tuple
   
-  >>> dostuff2("Dave",12)
+  >>> @check_args_ex((str,None),
+  ...                (int, (xrange(10),(5,9))),
+  ...                ((str,int),[('Jan','Feb','Mar','Apr','May')]))
+  ... def dostuff2(name, age, month):
+  ...     print "name = %s age = %d month = %s" % (name,age,month)
+ 
+  >>> dostuff2("Dave",5,'May')
+  name = Dave age = 5 month = May
+  
+  >>> dostuff2("Dave",12,'May')
   Traceback (most recent call last):
+      ...
   BadArgument: '12 is not in xrange(10)'
+  
+  >>> dostuff2("Dave",9,'Mayx')
+  Traceback (most recent call last):
+      ...
+  BadArgument: "Mayx is not in ('Jan', 'Feb', 'Mar', 'Apr', 'May')"
+
+  You can also use a function:
+
+  >>> def isvalid(arg):
+  ...   if arg in (1,2,5,"hello"):
+  ...     return True
+  ...   else:
+  ...     return False
+
+  >>> @check_args_ex(([],[isvalid]))
+  ... def dostuff3(greeting):
+  ...     print greeting
+
+  >>> dostuff3('hello')
+  hello
+  >>> dostuff3('goodbye')
+  Traceback (most recent call last):
+      ...
+  BadArgument: 'goodbye fails function isvalid'
+
   """
 
   def __init__(self, *args):
-    self.types = args
+    self.types, self.conditions = zip(*args)
+    
+    # the decorator depends on these being a list of lists (or tuples)...
+    self.types = [(i,) if type(i) not in (list, tuple) else i 
+                       for i in self.types]
+    self.conditions = [(i,) if type(i) not in (list, tuple) else i 
+                       for i in self.conditions]
 
-
-  conditions = {
-    xrange: lambda arg, condition: (bail("%s is not in %s" % (str(arg),str(condition))) if arg not in condition else 1),
-    list:   lambda arg, condition: (bail("%s is not in %s" % (str(arg),str(condition))) if arg not in condition else 1),
-    tuple:  lambda arg, condition: (bail("%s is not in %s" % (str(arg),str(condition))) if arg not in condition else 1),
-    type:   lambda arg, condition: (bail("%s is not of type %s" % (str(arg),str(condition))) if type(arg) != condition else 1)
+  condition_functions = {
+    types.FunctionType: lambda arg, condition: \
+                        (bail("%s fails function %s" % (str(arg),condition.func_name)) \
+                        if not condition(arg) else 1),
+    types.NoneType:     lambda arg, conditions: \
+                        True,
+    xrange:   lambda arg, condition: \
+              (bail("%s is not in %s" % (str(arg),str(condition))) 
+              if arg not in condition else 1),
+    list:     lambda arg, condition: \
+              (bail("%s is not in %s" % (str(arg),str(condition))) 
+              if arg not in condition else 1),
+    tuple:    lambda arg, condition: \
+              (bail("%s is not in %s" % (str(arg),str(condition))) 
+              if arg not in condition else 1)
   }
 
   def docondition(self, arg, condition):
-    self.conditions[type(condition)](arg,condition)
+    self.condition_functions[type(condition)](arg,condition)
+
+  def doconditionals(self, arg, conditionals):
+    [self.docondition(arg,conditional) for conditional in conditionals]
+
+  def dotypes(self, arg, types):
+    if len(types) and type(arg) not in types:
+      bail("%s is illegal type %s, legal types are: %s" % \
+           (str(arg), type(arg).__name__,
+            str([type(i).__name__ for i in types])))
 
   def __call__(self, function):
     def check_arguments(*args):
-      for arg, curtype in zip(args,self.types):
-        if type(curtype) in (tuple,list):
-          for condition in curtype:
-            self.docondition(arg,condition)
-        elif type(arg) != curtype:
-          raise BadArgument(str(arg) + " is not of type " + str(curtype)) 
+      [self.dotypes(arg,types) for arg, types in zip(args,self.types)]
+      [self.doconditionals(arg,cond) for arg, cond in zip(args,self.conditions)]
       function(*args)
     return check_arguments
 
