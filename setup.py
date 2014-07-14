1fe208b2cf594f23e3fcf5626479b0f9a078546d
David Case <Dav3xor@gmail.com>
2014-07-14 00:29:15 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup, Extension
+setup(name="pylws", version="1.0",
+      description='Decorators for checking argument types, range, etc',
+      author='David Case', author_email='Dav3xor@gmail.com',
+      url='http://www.github.com/Dav3xor/Python-CheckArgs/',
+      py_modules=['checkargs'],)
